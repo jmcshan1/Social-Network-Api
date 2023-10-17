75695320ef4c9562c68ff6abd8b1ce376e0370eb
jmcshan1 <jmcshan01@gmail.com>
2023-10-17 18:28:23 -0500

    added basic crud operations to user controller execpt for delete

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+// const { ObjectId } = require('mongoose').Types;
+const { User } = require('../models');
+
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getSingleUser(req, res){
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+              .select('-__v');
+      
+            if (!user) {
+              return res.status(404).json({ message: 'No user with that ID' });
+            }
+      
+            res.json(user);
+          } catch (err) {
+            res.status(500).json(err);
+          }
+    },
+    async createUser(req, res) {
+        try {
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
+          } catch (err) {
+            res.status(500).json(err);
+          }
+    },
+    async updateUser(req, res) {
+        try {
+            const filter = { _id: req.params.userId };
+            const update = (req.body)
+            const user = await User.findOneAndUpdate(filter, update, {
+                new : true
+            });
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // async deleteUser(req, res) {
+    // }
+}
